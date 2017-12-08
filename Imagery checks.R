d8919141365a3db449f88e760d20b3cb0642ef54
freyakeyser <freyakeyser@gmail.com>
2017-12-08 15:57:45 -0400

    shutting down for the weekend. plan to continue with caira's final files.

--- Imagery checks.R
+++ Imagery checks.R
@@ -5,8 +5,9 @@
 ### groundtruth corresponds to image files that Caira checked for mussel presence. 
 ### If the photo is in groundtruth, it truly exists and has an ID assigned. 
 cairaGT <- read.csv("Horse Mussel Groundtruthing_working1.csv", stringsAsFactors = F) 
+dim(cairaGT) #3202    5
 groundtruth <- read.csv("Horse Mussel Groundtruthing_final.csv", stringsAsFactors = F)
-
+dim(groundtruth) #3204    5
 ### so Caira added two rows to the final groundtruthing dataset. Let's keep going on this "final" version 
 
 groundtruth$Mussels. <- as.factor(groundtruth$Mussels.)
@@ -33,16 +34,16 @@ dim(groundtruth2011) # has 1431 rows
 ### these nav files are from the initial data dump from Kate (corresponds to files in Working Files 1)
 ### nav2009 and nav2011 are spatial metadata for the photos
 ### need to match the nav metadata to the groundtruth data so that we can identify mussel locations
-nav2009 <- read.csv("2009 CSV Format for R_working1.csv", stringsAsFactors = F) 
+caira2009 <- read.csv("2009 CSV Format for R_working1.csv", stringsAsFactors = F) 
 dim(caira2009) #1769   25
-nav2011 <- read.csv("2011 CSV Format for R_working1.csv", stringsAsFactors = F) 
+caira2011 <- read.csv("2011 CSV Format for R_working1.csv", stringsAsFactors = F) 
 dim(caira2011) #1348   25
 
 
 nav2009 <- read.csv("2009 CSV Format for R_final.csv", stringsAsFactors = F)
-dim(nav2009) #1764   25
+dim(nav2009) #1718   25
 nav2011 <- read.csv("2011 CSV Format for R_final.csv", stringsAsFactors = F)
-dim(nav2011) #1345   25
+dim(nav2011) #1287   25
 
 ### so caira removed 1769-1718 = 51 rows from the 2009 nav data
 ### and 1348-1287 = 61 rows from the 2011 nav data
@@ -56,7 +57,6 @@ nav2011$unid <- paste0(nav2011$STATION_NUM, "_", nav2011$PHOTO_FILE_NAME)
 dim(nav2009) # has 1769 rows
 dim(nav2011) # has 1348 rows
 
-
 #####################################################################################
 ### caira's remaining unresolved error lists are in "Y:/Caira/NRCAN Photo Data/Unresolved..."
 # 2009 has 48 error records (nav or groundtruth)
@@ -67,7 +67,7 @@ dim(nav2011) # has 1348 rows
 ### Inner join the photos and nav data (these records are presumably CLEAN)
 require(dplyr)
 join2009_clean <- inner_join(nav2009, groundtruth2009)
-dim(join2009_clean) # 1722   29
+dim(join2009_clean) # 1718   29
 join2011_clean <- inner_join(nav2011, groundtruth2011)
 dim(join2011_clean) # 1287   29
 
@@ -80,17 +80,17 @@ length(join2011_clean$unid[is.na(join2011_clean$ID.Date)])==0
 #####################################################################################
 ### full join the photos (df groundtruth2009/2011) and the nav data (df nav2009/2011)
 join2009_full <- full_join(nav2009, groundtruth2009) 
-dim(join2009_full) # 1822   29
-### so this means that there are 1822-1722 = 100 rows that are "missing" from one or the other
+dim(join2009_full) # 1773   29
+### so this means that there are 1773-1718 = 55 rows that are "missing" from one or the other
 
 join2011_full <- full_join(nav2011, groundtruth2011) 
-dim(join2011_full) # 1492   29
-### so this means that there are 1492-1287 = 205 rows that are "missing" from one or the other
+dim(join2011_full) # 1431   29
+### so this means that there are 1431-1287 = 144 rows that are "missing" from one or the other
 
 #####################################################################################
 ### left join the photos on the nav
 join2009_left <- left_join(nav2009, groundtruth2009)
-dim(join2009_left) #  1769   29
+dim(join2009_left) # 1718   29
 ### so this likely means that we have more groundtruth records (i.e. photos) than we have nav data, 
 ### meaning that nav records are missing
 
@@ -100,7 +100,7 @@ length(join2009_left$unid[is.na(join2009_left$ID.Date)])==0
 #####################################################################################
 ### right join the photos on the nav
 join2009_right <- right_join(nav2009, groundtruth2009)
-dim(join2009_right)
+dim(join2009_right) # 1773   29
 ### so we have more photos than nav data, and we can identify the photos without nav data 
 photoswithoutnav <- join2009_right[is.na(join2009_right$EXPED_CD),]
 
