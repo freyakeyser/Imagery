578f3c476b74ffbb49f1235eac62cf59076fccd5
freyakeyser <freyakeyser@gmail.com>
2017-12-19 14:06:43 -0400

    recovered station 9 nav2011 records

--- Imagery checks.R
+++ Imagery checks.R
@@ -201,35 +201,74 @@ clean2009 <- join2009_full[!is.na(join2009_full$EXPED_CD),]
 dim(clean2009) # 1759   29
 write.csv(clean2009, "clean2009.csv")
 
+
+
+### now we'll do the same checks for 2011. based on Caira's report, station 2 and 29 had issues. 
+### Let's identify the photos without nav data
 photoswithoutnav2011 <- join2011_full[is.na(join2011_full$EXPED_CD),]
 dim(photoswithoutnav2011) # 144 29
+View(photoswithoutnav2011)
+summary(photoswithoutnav2011) 
+table(photoswithoutnav2011$STATION_NUM)
 
+### let's focus on stations 9, 52, 69, 121 and 178
 
+## Caira noted that there were 55 nav records from station 29 that did not have photos. Let's check out these records to see if any patterns pop out.
+## need to look at the photo_file_names from photoswithoutnav2009 to be sure
+fromcairaslist <- as.character(c(291, 292, 293, 294, 295, 296, 297, 303, 304, 305, 306, 307, 308, 309, 310, 311, 312,
+ 313, 314, 315, 322, 323, 324, 325, 326, 327, 328, 329, 330, 331, 332, 333, 336,
+ 337, 338, 339, 340, 341, 342, 343, 344, 345, 346, 347, 348, 350, 351, 358, 359,
+ 362, 363, 364, 365, 366, 367))
+photoswithoutnav2011[photoswithoutnav2011$STATION_NUM == 29,] # none
+nav2011[nav2011$STATION_NUM == 29,] # none
+nav2011[nav2011$STATION_NUM == 9,] # none
+groundtruth2011[groundtruth2011$STATION_NUM == 9,] # 77 6
+unique(join2011_full$EXPED_CD[join2011_full$STATION_NUM==9]) #  all NA so station 9 is missing from full join
+unique(join2011_full$ID.Date[join2011_full$STATION_NUM==29]) #  all NA so station 29 is missing from full join
+## does it exist in original nav2011 df?
+caira2011[caira2011$STATION_NUM == 9,] # no records for station 9 in original nav data
+length(unique(caira2011$PHOTO_FILE_NAME[caira2011$STATION_NUM == 29])) # 55 records for station 29 in the ORIGINAL nav data
+groundtruth2011[groundtruth2011$STATION_NUM == 29,] # no photos labelled station 29 
+sort(unique(groundtruth2011$STATION_NUM)) # compared to the folder names in Photos/2011036. 55 Items in Station 0009. No station 29.
+length(unique(groundtruth2011$PHOTO_FILE_NAME[groundtruth2011$STATION_NUM == 9])) # 77. Why are there more records than there are photos? 
+### photos 298 - 302, 316 - 321, 334, 335, 349, 352-357, 360-361 are missing from folder!
+### 55 + 5 + 6 + 3 + 6 + 2 = 77
+### I suspect that the station_num 29 nav data corresponds to station_num 9 photos. Let's change caira2011 station_num 29 to 9, then rejoin.
 
+### a few final checks
+groundtruth2011[groundtruth2011$PHOTO_FILE_NAME %in% unique(caira2011$PHOTO_FILE_NAME[caira2011$STATION_NUM == 29]) & groundtruth2011$STATION_NUM == 9,]
+unique(photoswithoutnav2011$STATION_NUM[photoswithoutnav2011$PHOTO_FILE_NAME %in% fromcairaslist])
+### the photo_file_names all match
 
-# > dim(read.csv("Y:/Caira/NRCAN Photo Data/Data Checks/Old Data Check/2009 CSV Format for R.csv"))
-# [1] 1769  25
-# > dim(read.csv("Y:/Caira/NRCAN Photo Data/Data Checks/Old Data Check/2011 CSV Format for R.csv"))
-# [1] 1348  25
-# > dim(read.csv("Y:/Caira/NRCAN Photo Data/Data Checks/Working Files 1/2009 CSV Format for R_working1.csv"))
-# [1] 1769   25
-# > dim(read.csv("Y:/Caira/NRCAN Photo Data/Data Checks/Working Files 1/2011 CSV Format for R_working1.csv"))
-# [1] 1348   25
-# > dim(read.csv("Y:/Caira/NRCAN Photo Data/Data Checks/Working Files 2/2009 CSV Format for R_working2.csv"))
-# [1] 1764   25
-# > dim(read.csv("Y:/Caira/NRCAN Photo Data/Data Checks/Working Files 2/2011 CSV Format for R_working2.csv"))
-# [1] 1345   25
-# > dim(read.csv("Y:/Caira/NRCAN Photo Data/Data Checks/Working Files 3/2009 CSV Format for R_working3.csv"))
-# [1] 1718   25
-# > dim(read.csv("Y:/Caira/NRCAN Photo Data/Data Checks/Working Files 3/2011 CSV Format for R_working3.csv"))
-# [1] 1287   25
-# > dim(read.csv("Y:/Caira/NRCAN Photo Data/Data Checks/Final Files/2009 CSV Format for R_final.csv"))
-# [1] 1718   25
-# > dim(read.csv("Y:/Caira/NRCAN Photo Data/Data Checks/Final Files/2011 CSV Format for R_final.csv"))
-# [1] 1287   25
-# > dim(read.csv("Y:/Caira/NRCAN Photo Data/Data Checks/All errors removed/2009 CSV Format for R.csv"))
-# [1] 1764   25
-# > dim(read.csv("Y:/Caira/NRCAN Photo Data/Data Checks/All errors removed/2011 CSV Format for R.csv"))
-# [1] 1345   25
+nav2011 <- rbind(nav2011, caira2011[caira2011$STATION_NUM == 29,])         
+nav2011 <- nav2011[!is.na(nav2011$STATION_NUM),]
+length(nav2011$STATION_NUM[nav2011$STATION_NUM == 29]) == 55
 
+nav2011$STATION_NUM[nav2011$STATION_NUM == 29] <- 9
+nav2011$unid <- paste0(nav2011$STATION_NUM, "_", nav2011$PHOTO_FILE_NAME)
+
+### do the full join again for photos (df groundtruth2009) and the nav data (df nav2009)
+join2011_full <- full_join(nav2011, groundtruth2011) 
+dim(join2011_full) # 1431   29
+### so this means that there are 1431-1342=89 rows that are "missing" from one or the other
+### since there were 1431 records in groundtruth 2011, it is likely that there are more photos than nav data records
+length(join2011_full$unid[is.na(join2011_full$EXPED_CD)]) #89
+length(join2011_full$unid[is.na(join2011_full$ID.Date)]) #0
+### confirmed. we have 89 photos missing a nav record in 2011
+
+### now let's recreate photoswithoutnav2011 again
+photoswithoutnav2011 <- join2011_full[is.na(join2011_full$EXPED_CD),]
+dim(photoswithoutnav2011) # 89 29
+View(photoswithoutnav2011)
+summary(photoswithoutnav2011)
+table(photoswithoutnav2011$STATION_NUM) 
+
+### are these all valid station numbers?
+table(nav2011$STATION_NUM[nav2011$STATION_NUM %in% unique(photoswithoutnav2011$STATION_NUM)])
+### yes. so these photos may have issues with their photo_file_names. I am NOT going to bother trying to recover these 89 nav records as we have matched photo/nav for many photos
+### at these stations. Good enough. Let's move ahead without these 89 photos. 
+### CLEAN DF FOR 2011: 
+clean2011 <- join2011_full[!is.na(join2011_full$EXPED_CD),]
+dim(clean2011) # 1342   29
+write.csv(clean2011, "clean2011.csv")
 
